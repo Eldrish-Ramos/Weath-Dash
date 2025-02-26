d5df3273ef5cee03e935ada7577184054e874bb6
Eldrish <eldrish.ramosperez@gmail.com>
2025-02-26 12:26:47 -0800

    added .env | added functionality to weatherRoutes, weatherservice to be defined (in progress)

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,43 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req: Request, res: Response) => {
   // TODO: GET weather data from city name
+  const city = req.body.city;
+  WeatherService.getWeatherForCity(city)
+    .then((weather) => {
+      res.json(weather);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (_req: Request, res: Response) => {
+  try {
+    const history = await HistoryService.getCities();
+    res.status(200).json(history);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to get history' });
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    await HistoryService.removeCity(id);
+    res.status(200).json({ message: 'City deleted' });
+  }
+  catch (err) {
+    res.status(500).json({ error: 'Failed to delete city' });
+  }
+});
 
 export default router;
