8a5db659233b65eea744a60a7493312b3ff9416f
Eldrish-Ramos <44309851+Eldrish-Ramos@users.noreply.github.com>
2025-02-27 18:59:42 -0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
     open: true,
     proxy: {
       '/api': {
